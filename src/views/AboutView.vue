15333d24763e8ef3667e6d254922e8e7b4dbaad4
swabber <doutorking@gmail.com>
2022-03-13 10:10:53 +0900

    Add vuetify

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,14 +1,22 @@
 <template>
-  <div class="about">
     <h1>This is an about page</h1>
-  </div>
+    <div>
+      <v-btn color="primary" @click="clickButton">BUTTON TEST</v-btn>
+    </div>
 </template>
+
+<script setup lang="ts">
+function clickButton(e) {
+  console.log("button click");
+  console.log(e);
+}
+</script>
 
 <style>
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
-    display: flex;
+    display: contents;
     align-items: center;
   }
 }
